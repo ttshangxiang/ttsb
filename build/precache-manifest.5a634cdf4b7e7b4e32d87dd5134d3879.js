4aa44bc0a2e7b3cc8b729ff7c966156f7c489521
ttshangxiang <ttshangxiang@qq.com>
2019-11-20 19:30:24 +0800

    'build'

---
+++ build/precache-manifest.5a634cdf4b7e7b4e32d87dd5134d3879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c0360d2c9d7a026b04f441ee157118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929f500fba8e3b544aaa",
+    "url": "/static/css/2.d35c9eaf.chunk.css"
+  },
+  {
+    "revision": "2f01ec0ee56e2f1c96e1",
+    "url": "/static/css/main.1ad492d3.chunk.css"
+  },
+  {
+    "revision": "929f500fba8e3b544aaa",
+    "url": "/static/js/2.05ae4f03.chunk.js"
+  },
+  {
+    "revision": "2f01ec0ee56e2f1c96e1",
+    "url": "/static/js/main.8af1dfd3.chunk.js"
+  },
+  {
+    "revision": "45002424d97c0ca1a7fa",
+    "url": "/static/js/runtime-main.3a307f1f.js"
+  }
+]);
